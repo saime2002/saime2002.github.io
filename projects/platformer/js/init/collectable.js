fb59f0817ae4dc5d97ca01db3861fef957fbd22c
saime2002 <steward.aime2021@gmail.com>
2021-03-11 16:19:03 +0000

    add portfolio.html file

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -37,7 +37,7 @@
         // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
         
         // example: 
-        createCollectable(type.steve, 200, 170, 6, 0.7);
+        createCollectable(type.steve, 100, 170, 6, 0.7);
         createCollectable(type.kennedi, 100, 200, 6, 0.7);
         
         
